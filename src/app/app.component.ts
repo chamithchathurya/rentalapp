af4d6a54588a05b9d6aaf50690778758a5c6ae20
chamith chathurya <chamithchathurya00@gmail.com>
2024-12-04 16:31:31 +0530

    add rental

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'rentalapp';
+
+  constructor(private http:HttpClient){
+
+  }
+
+  public rental={
+     id:null,
+    rentalDate:null,
+    returnDate:null,
+    dueDate:null,
+    totalCost:null,
+    fine:null
+  }
+
+  add(){
+    console.log(this.rental);
+    this.http.post("http://localhost:8080/add-rental",this.rental).subscribe(data=>{
+      alert("rental Added")
+    })
+  }
 }
